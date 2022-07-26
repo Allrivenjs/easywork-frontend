4106ee5bcb643cbecc1045e03ae33af3fdd7e078
JAIME RUIZ <jaimeruizxdd2@gmail.com>
2022-07-26 04:08:39 -0500

    update

--- src/shared/services/chatServices/chatServices.test.ts
+++ src/shared/services/chatServices/chatServices.test.ts
@@ -1,7 +1,9 @@
 import { sendMessage } from "./";
-
+import {ISendMessage} from "../../../components/chat/interfaces";
+import {useCookies} from "react-cookie";
+const [cookies] = useCookies(["user-token"]);
 test("the data should be delivered", async () => {
-	const res = await sendMessage("token", "hello");
+	const res = await sendMessage(cookies["user-token"], {room_id: "1", message: "2"}  as ISendMessage);
 	console.log(res);
 	expect(res).not.toThrow(Error);
 });
