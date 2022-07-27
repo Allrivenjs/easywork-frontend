eee585d23ab896cd81b3acf10731b085caf189d9
alucard <faalsaru@gmail.com>
2022-07-26 23:29:34 -0500

    ts

--- src/components/payment/mercadopago/index.tsx
+++ src/components/payment/mercadopago/index.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { Test } from "./components/Test";
+import { Test } from "./components/Test.jsx";
 
 
 export const PaymentMercadoPagoRouter = () => {
